127d91f6075137d4df40f7fa5bfb414711a27b1d
Alexander Piilgaard <alex344b@edu.mercantec.dk>
2024-01-30 16:42:29 +0100

    Classes

---
+++ Classes/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace Classes
+{
+    public class CarMake
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Horsepower {  get; set; }
+        public string Engine { get; set; }
+        public string FuelType {  get; set; }
+        public int Price {  get; set; }
+        public string Color { get; set; }
+        public string KilometerCount { get; set; }
+    }
+}
---
+++ Classes/Class2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElBilClasses
+{
+    public class ElectricCars
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Horsepower { get; set; }
+        public int kWh { get; set; }
+        public string EngineCount { get; set; }
+        public int Price { get; set; }
+        public string Color { get; set; }
+        public string KilometerCount { get; set; }
+    }
+}
